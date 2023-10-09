c64331814300ae3f149365fcb5438145165d8b99
Taigo Ito <webmaster@qwel.design>
2023-10-09 18:31:57 +0900

    Temporarily implement the main visual slider

--- js/init.js
+++ js/init.js
@@ -10,3 +10,7 @@ import { applicationId, shopCode } from './config.js';
 // Products
 import Products from './_products.js';
 new Products(`https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601?format=json&shopCode=${shopCode}&applicationId=${applicationId}`);
+
+// Slider
+import Slider from './_slider.js';
+new Slider();
